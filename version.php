bb23cc41ef86bb2df306eea76064ea8a193d2740
John Syrinek <john.syrinek@gmail.com>
2013-01-09 16:10:38 -0600

    Initial commit

---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Version details
+ *
+ * @package    block
+ */
+
+$plugin->version   = 2013010900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012112900;        // Requires this Moodle version
+$plugin->component = 'local_jquery';
+
+global $PAGE;
+$PAGE->requires->js('/local/jquery/jquery-1.8.3.min.js');
